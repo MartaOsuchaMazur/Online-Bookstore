8b195aa4b10c36c78c22f0cef7cc379cc2079f52
MartaOsuchaMazur <jagooo1@wp.pl>
2023-03-11 21:27:16 +0100

    M2L4 - Dodanie pierwszego beana

--- src/main/java/pl/sztukakodu/bookmmr/OnlineBookstoreApplication.java
+++ src/main/java/pl/sztukakodu/bookmmr/OnlineBookstoreApplication.java
@@ -1,13 +1,29 @@
 package pl.sztukakodu.bookmmr;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+import java.util.List;
+
 @SpringBootApplication
-public class OnlineBookstoreApplication {
+public class OnlineBookstoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBookstoreApplication.class, args);
 	}
 
+	private final CatalogService catalogService;
+
+	public OnlineBookstoreApplication(CatalogService catalogService) {
+		this.catalogService = catalogService;
+	}
+
+	@Override
+	public void run(String... args) {
+	//	CatalogService service = new CatalogService();
+		List<Book> books = catalogService.findByTitle("Pan Tadeusz");
+		books.forEach(System.out::println);
+	}
 }
